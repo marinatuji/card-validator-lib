6aaffda25ef879b0f29e99ed2957a67df20069f6
marinatuji <mtujimura@gmail.com>
2019-05-08 04:53:04 -0300

    JS: refatoração de código e calculo com bug na somatoria

--- test/index.spec.js
+++ test/index.spec.js
@@ -3,17 +3,23 @@ const cardValidator = require('../lib/index');
 
 describe('cardValidator()', () => {
   describe('Número de cartão válido:', () => {
+    it('Retorna true: ', () => {
+      expect(cardValidator(8532)).to.equal(true);
+    });
     it('Retorna true: Entrada válida de 16 dígitos', () => {
-      expect(cardValidator(4984235062514488)).to.equal(false);
+      expect(cardValidator(4984235062514488)).to.equal(true);
+    });
+    it('Retorna true: Entrada válida como string', () => {
+      expect(cardValidator('4984235062514488')).to.equal(true);
+    });
+  });
+
+  describe('Número de cartão inválido:', () => {
+    it('Retorna false: Cartão inválido', () => {
+      expect(cardValidator('4984 2350 6251 4489')).to.equal(false);
     });
     it('Retorna false: Entrada inválida (menor que 16 dígitos)', () => {
       expect(cardValidator(49842350)).to.equal(false);
     });
-  })
-  
-  describe('Número de cartão inválido:', () => {
-    it('Retorna false: Cartão inválido', () => {
-      expect(cardValidator()).to.equal(false);
-    });
-  })
+  });
 })
