aadf9c23b5f95c8db14b6c3f4f5d95291140b445
marinatuji <mtujimura@gmail.com>
2019-05-08 10:57:38 -0300

    js: refatoração

--- test/index.spec.js
+++ test/index.spec.js
@@ -3,8 +3,8 @@ const cardValidator = require('../lib/index');
 
 describe('cardValidator()', () => {
   describe('Número de cartão válido:', () => {
-    it('Retorna true: ', () => {
-      expect(cardValidator(8532)).to.equal(true);
+    it('Retorna false: Entrada válida menor que 16 digitos', () => {
+      expect(cardValidator(8532)).to.equal(false);
     });
     it('Retorna true: Entrada válida de 16 dígitos', () => {
       expect(cardValidator(4984235062514488)).to.equal(true);
@@ -13,13 +13,16 @@ describe('cardValidator()', () => {
       expect(cardValidator('4984235062514488')).to.equal(true);
     });
   });
-
+  
   describe('Número de cartão inválido:', () => {
     it('Retorna false: Cartão inválido', () => {
       expect(cardValidator('4984 2350 6251 4489')).to.equal(false);
     });
     it('Retorna false: Entrada inválida (menor que 16 dígitos)', () => {
       expect(cardValidator(49842350)).to.equal(false);
+    });
+    it('Retorna false: Entrada inválida (tipo booleano)', () => {
+      expect(cardValidator(true)).to.equal(false);
     });
   });
 })
